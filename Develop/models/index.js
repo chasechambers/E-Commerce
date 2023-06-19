86a8def01124e73b8fff18fbd72a849859445e71
Chase <chambers.chase@gmail.com>
2023-06-19 14:49:23 -0400

    Building routes

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -5,25 +5,23 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category, {
-  foreignKey: 'product_id',
-});
+Product.belongsTo(Category);
 
 // Categories have many Products
 Category.hasMany(Product, {
-foreignKey: 'cateogry_id',
+foreignKey: 'category_id',
 });
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
   through: 'ProductTag',
-  foreignKey: 'product_id',
+  foreignKey: 'tag_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
   through: 'ProductTag',
-  foreignKey: 'Tag_id',
+  foreignKey: 'tag_id',
 });
 
 module.exports = {
--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -3,9 +3,17 @@ const { Tag, Product, ProductTag } = require('../../models');
 
 // The `/api/tags` endpoint
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
   // find all tags
   // be sure to include its associated Product data
+  try {
+    const tagLists = await Tag.findAll({
+      include: [{ model: Category}, {model: ProductTag}, {model: Product}], 
+    });
+    res.status(200).json(tagLists);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.get('/:id', (req, res) => {
