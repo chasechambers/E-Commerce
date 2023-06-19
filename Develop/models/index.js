6524b1d87917f0565ea11fd23dae096925022ea3
Chase <chambers.chase@gmail.com>
2023-06-19 16:40:08 -0400

    Working on code for tags

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,7 +6,10 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
-Product.belongsTo(Category);
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 
 // Categories have many Products
 Category.hasMany(Product, {
@@ -15,12 +18,14 @@ foreignKey: 'category_id',
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: 'ProductTag'
+  through: 'ProductTag',
+  foreignKey: 'product_id'
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: 'ProductTag'
+  through: 'ProductTag',
+  foreignKey: 'tag_id'
 });
 
 module.exports = {
