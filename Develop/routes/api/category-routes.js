fd18fc115693d87b0f3c6f85dabd9cc1efe86144
Chase <chambers.chase@gmail.com>
2023-06-19 16:12:30 -0400

    Working on belongstomany language

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -48,8 +48,11 @@ router.post('/', async (req, res) => {
 
 router.put('/:id', async (req, res) => {
   // update a category by its `id` value
-  
-
+//   Category.update(
+//     {category_name: req.body.category_name},
+//     {where: {id: req.params.id}}
+//   )
+// .then(function())
 });
 
 router.delete('/:id', async (req, res) => {
